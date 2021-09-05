13303db9fe25be2435e7fb37d5b2eec7d82105bb
Belmy Semape Maripane <87295997+bsmaripane@users.noreply.github.com>
2021-09-05 22:57:55 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Primitive_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Belmy Semape Maripane\n");
+
+            Console.WriteLine("What is your name(s)?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is yous surname?");
+            var surname = Console.ReadLine();
+
+            var currentDate = DateTime.Now;
+
+            Console.WriteLine($"\nHello\nName: {name}\nSurname: {surname}\n\nOn {currentDate:d} at {currentDate:t}!");
+            
+            Console.WriteLine($"\nEND OF THE PROGRAM!...    Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
